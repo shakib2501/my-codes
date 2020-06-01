6ac63a9f2b02e8919b5ee1c8710e8d715fd989a3
shakib2501 <shakib2501@gmail.com>
2020-06-02 00:21:42 +0530

    longWords.java is submitted successfully

--- longWords.java
+++ longWords.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
 public class longWords {
+	static Scanner sr = new Scanner(System.in);
 	public String tester (String str) {
-		return null;
+		int len = str.length();
+		String result = "";
+		if (len <= 10) 
+			result = str;
+		else {
+			result = result + str.charAt(0) + Integer.toString(len - 2) + str.charAt(len - 1);
+		}
+		return result;
 	}
 	public static void main (String[] args) {
-		
+		longWords obj = new longWords();
+		double length = sr.nextInt();
+		String in = null;
+		for (int i = 0; i <= length; i++) {
+			in = sr.nextLine();
+			System.out.println(obj.tester(in));
+		}
 	}
 }
