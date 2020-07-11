35dd95f841dadce58edfc65f6895f1db03e7136d
shakib2501 <shakib2501@gmail.com>
2020-07-11 11:21:11 +0530

    resolving commit

---
+++ oddSelection.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public oddSelection {
+	public boolean helper (ArrayList arr, int x) {
+		for (int i = 0; i < arr.size(); i++) {
+
+		}
+	}
+	static Scanner sr = new Scanner(System.in);
+	public static void main (String[] args) {
+		int tests = sr.nextInt();
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		for (int i = 0; i < tests; i++) {
+			arr.clear();
+			int arrlen = sr.nextInt();
+			int x = sr.nextInt();s
+			for (int j = 0; j < arrlen; j++) {
+				arr.add(sr.nextInt());
+			}
+			System.out.println(obj.helper(arr,x));
+		}
+	}
+}
